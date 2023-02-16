9f7ff48b7932015fa182e07866b3ea73c6f64ffb
xufanglu <3146974+likev@users.noreply.github.com>
2023-02-16 08:10:02 +0000

    basic chat

--- src/config.js
+++ src/config.js
@@ -1,57 +1,6 @@
 const CONFIG = {
-    'medium-uv-rh': {
-        levels: [1000, 925, 850, 700, 500, 300, 250, 200]
-    },
 
-    'medium-uv-z': {
-        levels: [1000, 925, 850, 700, 500, 300, 250, 200]
-    },
-    'medium-divergence': {
-        levels: [1000, 925, 700, 500, 300],
-        name: 'divergence'
-    },
-    'medium-vorticity': {
-        levels: [850, 700, 500, 300, 250],
-        name: 'vorticity'
-    },
-    'medium-pv': {
-        levels: [500, 300],
-        name: 'pv'
-    },
-    'medium-wind-10wg': {
-        levels: [1000],
-        name: 'medium-wind-10wg'
-    },
-    'medium-shear': {
-        levels: [1000],
-        name: 'medium-shear'
-    },
-    'medium-precipitation-type': {
-        levels: [1000],
-        name: 'medium-precipitation-type'
-    },
-    'medium-rain-acc': {
-        levels: [1000],
-        name: 'medium-rain-acc'
-    },
-    'medium-rain-rate': {
-        levels: ['tp_rate', 'lsp_rate', 'cp_rate', 'sf_rate'],
-        name: ''
-    },
-    'medium-visibility': {
-        levels: [1000],
-        name: 'medium-visibility'
-    },
-    'opencharts_meteogram': {
-        levels: ['classical_15d_with_climate', 'classical_wave', 'classical_10d', 'classical_plume', 'classical_15d'],
-        name: 'Luoyang',
-        type: 'point-based'
-    },
-    'opencharts_vertical-profile-meteogram': {
-        levels: ['opencharts_vertical-profile-meteogram'],
-        name: 'Luoyang',
-        type: 'point-based-profile'
-    },
+    "BINGAI-PROXY": "https://fragile-seal-cuff.cyclic.app/conversation"
 };
 
 export default CONFIG;
--- src/main.js
+++ src/main.js
@@ -11,171 +11,61 @@ window.bootstrap = bootstrap;
 import config from './config';
 import { fixedEncodeURIComponent, getUTCTimeStr } from "./utils"
 
-const currentHour = moment().hour();
-let minusday = currentHour >= 15 ? 0 : 1;
-let basehour = (currentHour >= 15 || currentHour <= 5) ? 8 : 20;
+let promot = "";
+let conversationInfo = {};
 
-let basetime = moment().hour(basehour).subtract(minusday, 'days');
-let validtime = basetime.clone().add(12, 'hours');
+async function getAnswer(fetch_body) {
 
-
-let productType = 'medium-uv-rh';
-let level = '500';
-
-async function getProduct() {
-
-    //const openchartsApi = 'https://charts.ecmwf.int/opencharts-api/v1/export/';
-    const openchartsApi = 'https://ecmwf-apps.tianqitu.net/opencharts-api/v1/export/';
-
-    //const cors_api = 'https://icors.vercel.app/';
-
-    const productConfig = config[productType];
-
-    let fetch_body = {
-        "package": "opencharts",
-        "product": productType,
-        "format": "png",
-        "base_time": getUTCTimeStr(basetime),
-        "valid_time": getUTCTimeStr(validtime),
-        
-    };
-
-    if (productConfig.type) {
-
-        if (productConfig.type === 'point-based') {
-            fetch_body.epsgram = level;
-            fetch_body.station_name = productConfig.name;
-            fetch_body.lat = '34.6836';
-            fetch_body.lon = '112.454';
-        }else if(productConfig.type === 'point-based-profile'){
-            fetch_body.station_name = productConfig.name;
-            fetch_body.lat = '34.6836';
-            fetch_body.lon = '112.454';
-        }
-    }else{
-        fetch_body.level = level;
-        fetch_body.projection = "opencharts_eastern_asia";
-    }
-
-    //var ajax_api = `${cors_api}?${fixedEncodeURIComponent(fetch_url)}`;
-
-    let f = await fetch(openchartsApi, {
-        "body": JSON.stringify(fetch_body),
+    let f = await fetch(config['BINGAI-PROXY'], {
+        "headers": {
+            "content-type": "application/json; charset=utf-8",
+        },
+        "body": JSON.stringify({...conversationInfo, ...fetch_body}),
         "method": "POST",
         "mode": "cors"
     });
 
+    /*
+    {
+    "message": "Hello, how are you today?",
+    "conversationId": "your-conversation-id (optional)",
+    "parentMessageId": "your-parent-message-id (optional, for `ChatGPTClient` only)",
+    "conversationSignature": "your-conversation-signature (optional, for `BingAIClient` only)",
+    "clientId": "your-client-id (optional, for `BingAIClient` only)",
+    "invocationId": "your-invocation-id (optional, for `BingAIClient` only)",
+}
+
+// HTTP/1.1 200 OK
+{
+    "response": "I'm doing well, thank you! How are you?",
+    "conversationId": "your-conversation-id",
+    "messageId": "response-message-id (for `ChatGPTClient` only)",
+    "conversationSignature": "your-conversation-signature (for `BingAIClient` only)",
+    "clientId": "your-client-id (for `BingAIClient` only)",
+    "invocationId": "your-invocation-id (for `BingAIClient` only - pass this new value back into subsequent requests as-is)",
+    "details": "additional details about the AI's response (for `BingAIClient` only)"
+}
+    */
     let result = await f.json();
 
-    console.log(result)
+    let {clientId, conversationId, conversationSignature, invocationId} = result;
+    conversationInfo = {clientId, conversationId, conversationSignature, invocationId};
 
-    return result.url;
+    return result;
 }
 
-async function refresh() {
-    $('#chart-spinner').show();
-    //$('#result .chart').hide();
+$('#submit-ask').on('click', async function(){
+    const ask = $("#ask-content").val().trim();
+    let fetch_body = {"message": promot + ask};
+    
+    //<div class="ask">This is some text within a card body.</div>
+    //<div class="answer">This is some text within a card body.</div>
+    $('#chat-content').append(`<div class="ask">${ask}</div>`);
 
-    $('#result .chart').attr('src', '#');
-    let link = await getProduct();
-    $('#result .chart').attr('src', link);
-}
+    let result = await getAnswer(fetch_body);
+    
+    let {response} = result;
 
-$('#result .chart').on('load', function () {
-    $('#chart-spinner').hide();
-    //$('#result .chart').show();
+    $('#chat-content').append(`<div class="answer">${response}</div>`);
+
 })
-
-function update_levels() {
-    const levels = config[productType].levels;
-
-    const list = $('#levels');
-    list.html('');
-
-    let contain = false;
-
-    levels.forEach(function (val) {
-
-        let active_str = ` " `;
-        if (val + '' === level) {
-            contain = true;
-            active_str = ` active" aria-current="true" `;
-        }
-
-        list.prepend(`<a href="#" data-level='${val}' class="list-group-item list-group-item-action ${active_str}>
-            ${val}
-          </a>`);
-
-    })
-
-    if (!contain) {
-        list.find('a').eq(0).trigger('click');
-    }
-}
-
-function update_validdates() {
-
-
-    const list = $('#validdates');
-    list.html('');
-
-    for (let span = 0; span <= 168; span += 12) {
-        let date = basetime.clone().add(span, 'hours');
-
-        list.append(`<li class="page-item ${date.isSame(validtime) ? 'active' : ''}" aria-current="page">
-            <a class="page-link" href="#" data-utc='${getUTCTimeStr(date)}'  data-local='${+date}'><span class="fs-4">${date.date()}</span><span class="fs-6">${date.format("HH")}</span></a>
-          </li>`)
-
-    }
-}
-
-update_validdates();
-
-$('#validdates').on('click', '.page-link', function () {
-    const time = $(this).data('local');
-    validtime = moment(time);
-
-    update_validdates();
-
-    refresh();
-})
-
-$('#levels').on('click', 'a', function () {
-    level = $(this).data('level') + '';
-    update_levels();
-
-    refresh();
-})
-
-$('#select-products').on('change', function () {
-    productType = $(this).val();
-    update_levels();
-
-    const productConfig = config[productType];
-    if (productConfig.type && productConfig.type === 'point-based') {
-        $('#validdates').html('');
-    } else {
-        update_validdates();
-    }
-
-    refresh();
-});
-
-//refresh();
-
-function set_basetime() {
-    $('#basetime').val('起始场 ' + basetime.format('YYYY-MM-DD HH') + ' 北京时');
-    $('#select-products').trigger('change');
-}
-
-$('#prevtime').on('click', function () {
-    basetime.subtract(12, 'hours');
-    set_basetime();
-})
-
-$('#nexttime').on('click', function () {
-    basetime.add(12, 'hours');
-    set_basetime();
-})
-
-set_basetime();
